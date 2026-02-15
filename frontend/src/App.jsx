7c7829aae723a965ebac38c5d2b5ed9f6c09202a
L4NR3 <owolabilanre2006@gmail.com>
2026-02-15 22:59:31 +0100

    i made some git commit mistakes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
+    const [message, setMessage] = useState("");
 
-  return (
-    <>
-    </>
-  )
+    useEffect(() => {
+          fetch("http://localhost:5000")
+            .then(res => res.text())
+            .then(data => setMessage(data));
+        }, []);
+
+    return (
+          <div>
+            <h1>Frontend Connected to Backend</h1>
+            <p>{message}</p>
+          </div>
+        );
 }
-export default App
+
+export default App;
+
